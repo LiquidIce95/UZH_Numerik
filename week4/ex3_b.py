3a725887ad4af259638965ecaee563fc8beb2821
LiquidIce95 <davidsanchez062@gmail.com>
2024-03-19 10:52:14 +0100

    solved 3b, prepared 3c

--- week4/ex3_b.py
+++ week4/ex3_b.py
@@ -1,7 +1,27 @@
 from week4.ex3_a import AcomputeDiffOp,BcomputeDiffOp,CcomputeDiffOp
 from matplotlib import pyplot as plt
 
-def computeValues(f:callable,h:float,x0:float)->list:
+def DiffOpsA(f:callable,x0:float,h:float):
+    fiA = [f(x0-2*h+j*h) for j in range(0,3)]
+    return AcomputeDiffOp(fiA,h)
+
+def DiffOpsB(f:callable,x0:float,h:float):
+    fiB = [f(x0-h + j*h) for j in range(0,4)]
+    return BcomputeDiffOp(fiB,h)
+
+def DiffOpsC(f:callable,x0:float,h:float):
+    fiC = [f(x0-h + j*h) for j in range(0,2)]
+    return CcomputeDiffOp(fiC,h)
+
+"""we write this do reuse in next sub exercise"""
+def computeValue(f:callable,h:float,x0:float,do:callable)->float:
+    """
+    do is the difference operator
+    """
+    return do(f,x0,h)
+
+
+def computeValuesABC(f:callable,h:float,x0:float)->list:
     """
     @f function whose derivative to approximate
     @h increment of point districution
@@ -9,16 +29,7 @@ def computeValues(f:callable,h:float,x0:float)->list:
     @returns tupel with the values for diff operator A,B,C
     """
 
-    fiA = [f(x0-2*h+j*h) for j in range(0,3)]
-    AValue = AcomputeDiffOp(fiA,h)
-
-    fiB = [f(x0-h + j*h) for j in range(0,4)]
-    BValue = BcomputeDiffOp(fiB,h)
-
-    fiC = [f(x0-h + j*h) for j in range(0,2)]
-    CVAlue = CcomputeDiffOp(fiC,h)
-
-    return AValue,BValue,CVAlue
+    return computeValue(f,h,x0,DiffOpsA),computeValue(f,h,x0,DiffOpsB),computeValue(f,h,x0,DiffOpsC)
 
 def computeErrors(f:callable,h:float,x0:float,d:callable)->list:
     """
@@ -28,7 +39,7 @@ def computeErrors(f:callable,h:float,x0:float,d:callable)->list:
     @d is the derivative of f
     @returns tupel with the Errors for diff operator A,B,C
     """
-    Value = computeValues(f,h,x0)
+    Value = computeValuesABC(f,h,x0)
 
     Errors = [abs(V - d(x0)) for V in Value]
 
@@ -36,14 +47,43 @@ def computeErrors(f:callable,h:float,x0:float,d:callable)->list:
 
 
 
-def plotErrors(values :list)->None:
+def plotErrors(xValues:list,yValues :list)->None:
     """
     @values tupesl (error,h) which will be plotted
     """
-    yValues = [val[0] for val in values]
-    xValues = [val[1] for val in values]
-    
+    assert len(xValues)==len(yValues)
+
+    plt.plot(xValues,yValues,label="Error with repsect to h")
+    plt.title('Error of difference operator')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+def SolveTask()->None:
+    x0 = 10
+    H = [2**(-n) for n in range(0,11)]
+    f = lambda x : 1/(0.4*x)
+    d = lambda x : (-1)/(0.4*x**2)
+
+    """your values as triplets 0 is a, 1 is b, 2 is c
+        where a,b,c are the respective diff operators"""
+    Values = [computeValuesABC(f,h,x0) for h in H]
+
+    Errors = [computeErrors(f,h,x0,d) for h in H]
+
+    yErrorsA = [Err[0] for Err in Errors]
+    yErrorsB = [Err[1] for Err in Errors]
+    yErrorsC = [Err[2] for Err in Errors]
+
+    plotErrors(H,yErrorsA)
+    plotErrors(H,yErrorsB)
+    plotErrors(H,yErrorsC)
+
+
 
 
 if __name__ == "__main__":
-    pass
+    SolveTask()
