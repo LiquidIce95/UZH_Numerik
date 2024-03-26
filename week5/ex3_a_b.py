88128c877c5c6e4681c6dc000416af913841d75b
LiquidIce95 <davidsanchez062@gmail.com>
2024-03-26 10:37:20 +0100

    solved all exercises except the conclusions part

---
+++ week5/ex3_a_b.py
@@ -0,0 +1,82 @@
+from week5.ex2_b import *
+from math import e
+
+def equidistantPoints(a:float,b:float,n:int)->list:
+    """
+    @a left edge of intervall
+    @b right edge of intervall
+    @n number of subintervalls
+    @returns the equidistant points which make the subintervalls
+
+    """
+    points = [a]
+    h = (b-a)/n
+    for j in range(1,n+1):
+        points.append(points[j-1]+h)
+
+    return points
+
+def midpoint_composite(f:callable,L:float,R:float,n:int)->float:
+    """
+    @f function whose integral is approximated
+    @L left edge of intervall
+    @R right edge of intervall
+    @n number of subintervalls
+    @returns the approximated integral
+    """
+
+    Points = equidistantPoints(L,R,n)
+
+    Integrals = [midpoint_rule(f,Points[j],Points[j+1]) for j in range(n)]
+
+    return sum(Integrals)
+
+
+def trapezoidal_composite(f:callable,L:float,R:float,n:int)->float:
+    """
+    @f function whose integral is approximated
+    @L left edge of intervall
+    @R right edge of intervall
+    @n number of subintervalls
+    @returns the approximated integral
+    """
+
+    Points = equidistantPoints(L,R,n)
+
+    Integrals = [trapezoidal_method(f,Points[j],Points[j+1]) for j in range(n)]
+
+    return sum(Integrals)
+
+def simpsons_composite(f:callable,L:float,R:float,n:int)->float:
+    """
+    @f function whose integral is approximated
+    @L left edge of intervall
+    @R right edge of intervall
+    @n number of subintervalls
+    @returns the approximated integral
+    """
+
+    Points = equidistantPoints(L,R,n)
+
+    Integrals = [trapezoidal_method(f,Points[j],Points[j+1]) for j in range(n)]
+
+    return sum(Integrals)
+
+
+if __name__ == "__main__":
+    f = lambda x : e**x
+    a,b = 0,1
+    n = 100
+    I = e-1
+
+    ErrorMidpoint = abs(I-midpoint_composite(f,a,b,n))
+    ErrorTrapezoidal = abs(I-trapezoidal_composite(f,a,b,n))
+    ErrorSimpsons = abs(I-simpsons_composite(f,a,b,n))
+
+    print(ErrorMidpoint,ErrorSimpsons,ErrorSimpsons)
+
+    """
+    Error of midpoint is smallest, since e**x on subintervalls is close to x
+    which can be best approximated with midoint (area of triangle)
+    Errors of Trapezoidal and Simposons are equal
+    """
--- week5/ex3_c.py
+++ week5/ex3_c.py
@@ -1,18 +1,23 @@
-from week5.ex2_b import midpoint_rule,simpsons_method,trapezoidal_method
-import math
+from week5.ex3_a_b import *
 from matplotlib import pyplot as plt
+from math import e
 
 if __name__ == "__main__":
     a = 0
-    B = [2**(-n) for n in range(1,8)]
-    f = lambda x : math.e**x
-    ErrorMidpoint = [abs((math.e -1)- midpoint_rule(f,a,b)) for b in B]
-    ErrorSimpson = [abs((math.e -1)- simpsons_method(f,a,b)) for b in B]
-    ErrorTrapezoidal = [abs((math.e -1)- trapezoidal_method(f,a,b)) for b in B]
+    B = [2**(n) for n in range(1,10)]
+    f = lambda x : e**x
+    Integral = e-1
+
+    ErrorMidpoint = [abs(Integral - midpoint_rule(f,a,b)) for b in B]
+    ErrorSimpson = [abs(Integral - simpsons_method(f,a,b)) for b in B]
+    ErrorTrapezoidal = [abs(Integral - trapezoidal_method(f,a,b)) for b in B]
+
+    refFunction = [1/m for m in B]
 
     plt.plot(B,ErrorMidpoint,label="Error of midpoint with repsect to h")
     plt.plot(B,ErrorSimpson,label="Error simposons with repsect to h")
     plt.plot(B,ErrorTrapezoidal,label="Error trapezoidal with repsect to h")
+    plt.plot(B,refFunction,label="reference function")
 
     plt.title('Error of difference operator of h')
     plt.xlabel('distance h')
@@ -23,6 +28,4 @@ if __name__ == "__main__":
     plt.grid(True)
     plt.show()
 
-    """
-    conclusions
-    """
+    """Conclusions"""
